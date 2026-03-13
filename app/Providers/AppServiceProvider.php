bc6021f5a0787b93f4795cea272949329ed525be
Raswanth <raswanthsabarish@gmail.com>
2026-03-13 11:25:20 +0530

    Update production environment check in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,11 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
-        if (config('app.env') === 'production') {
-=======
         if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
->>>>>>> 6a759e5036fda1035cc0c063e54488392a0fa321
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
